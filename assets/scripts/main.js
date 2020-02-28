ba669f06d3ae72e701539ad520bd686fedf42419
GitJay37 <jeisondejesus@hotmail.es>
2020-02-28 16:08:52 -0500

    boletines project

---
+++ assets/scripts/main.js
@@ -0,0 +1,24 @@
+// Obtener elementos de con clase .close
+let links = document.querySelectorAll(".close");
+// Recorrer elementos
+links.forEach(function(link){
+    
+    // Agregar eventos
+    link.addEventListener("click", function(e){
+        e.preventDefault();
+
+        let content = document.querySelector(".content");
+
+        // Eliminar classes de animation
+        content.classList.remove("fadeInDown");
+        content.classList.remove("animated");
+        // Agregar clases de animation 
+        content.classList.add("fadeOutUp");
+        content.classList.add("animated");
+
+        setTimeout(function(){
+            location.href = "/";
+        },600);
+        
+    });
+});
